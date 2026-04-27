786177b00caab3a45a246589d7961bb75a819654
jklins-ects <73347118+jklins-ects@users.noreply.github.com>
2026-04-27 08:06:53 -0400

    removed extra activities

--- src/utils/activityMetadata.js
+++ src/utils/activityMetadata.js
@@ -3,12 +3,12 @@ const ACTIVITY_METADATA = [
     { key: "activity1", title: "Drone Piloting" },
     { key: "activity2", title: "Robot Programming" },
     { key: "activity3", title: "Model Airplane Piloting" },
-    { key: "activity4", title: "Temp Activity Title 4" },
-    { key: "activity5", title: "Temp Activity Title 5" },
-    { key: "activity6", title: "Temp Activity Title 6" },
-    { key: "activity7", title: "Temp Activity Title 7" },
-    { key: "activity8", title: "Temp Activity Title 8" },
-    { key: "activity9", title: "Temp Activity Title 9" },
+    { key: "activity4", title: "Circuit Creation" },
+    { key: "activity5", title: "" },
+    { key: "activity6", title: "" },
+    { key: "activity7", title: "" },
+    { key: "activity8", title: "" },
+    { key: "activity9", title: "" },
 ];
 
 const ACTIVITY_KEYS = ACTIVITY_METADATA.map((activity) => activity.key);
